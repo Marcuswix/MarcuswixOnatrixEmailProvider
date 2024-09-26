fe5fcf51652c6dac0f41b384c71e27f26735db58
Marcus <Marcuswix@yahoo.se>
2024-09-26 09:53:31 +0200

    "Done"

---
+++ OnatrixEmailProvider/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using Azure.Communication.Email;
+using Microsoft.Extensions.Configuration;
+
+[assembly: FunctionsStartup(typeof(MyNamespace.Startup))]
+
+namespace MyNamespace
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Registrera EmailClient som en Singleton
+            builder.Services.AddSingleton<EmailClient>(provider =>
+            {
+                var configuration = provider.GetRequiredService<IConfiguration>();
+                var connectionString = configuration["CommunicationServices"];
+                return new EmailClient(connectionString);
+            });
+
+            // Lägg till andra tjänster om det behövs
+        }
+    }
+}
+
